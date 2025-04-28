90b0ef1c41287b44d25ca17be89d3f922113c140
Justin Hsiao <hsiaojustin41@gmail.com>
2025-04-28 14:09:27 +1200

    added base files

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -1,7 +1,19 @@
 from app import app
-from flask import render_template, abort
+from flask import render_template, abort, request
 from flask_sqlalchemy import SQLALCHEMY
 import os
 
-if __name__ == "__main__":
-    app.run(debug=True)
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "draft.db")
+db.init_app(app)
+
+import app.models as models
+
+app.route('/')
+def layout():
+    return render_template("layout.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
