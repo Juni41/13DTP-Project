54e8c5d573b225a3ad684fd7d4d9a3c7c366f0b8
Justin Hsiao <hsiaojustin41@gmail.com>
2025-05-19 19:25:35 +1200

    moved from routes

--- Project/app/__init__.py
+++ Project/app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///draft.db'  
 
-from app import routes
+db = SQLAlchemy(app)
 
-app.run(debug=True)
+
+from app import routes, models
+
+with app.app_context():
+    from app import models 
