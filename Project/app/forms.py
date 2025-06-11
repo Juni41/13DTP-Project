445d6fe2ee40a985149c5ebe74fd223382283f78
Justin Hsiao <hsiaojustin41@gmail.com>
2025-06-11 20:12:38 +1200

    switching to flask_wtf

---
+++ Project/app/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length
+
+class PlayerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=30)])
+    skill = SelectField('Skill Level', choices=[
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced')
+    ], validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], validators=[DataRequired()])
+    submit = SubmitField('Add Player')
+
+class MatchForm(FlaskForm):
+    num_courts = IntegerField('Number of Courts', validators=[DataRequired()])
+    method = SelectField('Sorting Method', choices=[
+        ('random', 'Random'),
+        ('skill', 'Balance by Skill'),
+        ('gender', 'Balance by Gender')
+    ], validators=[DataRequired()])
+    submit = SubmitField('Generate Matches')
