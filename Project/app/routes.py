5725cb2ca141d37ca3b1b54b25ddebe2eb594769
Justin Hsiao <hsiaojustin41@gmail.com>
2025-06-27 12:08:45 +1200

    added stats for total amount of players and courts needed

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -3,6 +3,8 @@ from flask import render_template, abort, request, redirect, url_for
 from app.models import Player, Match, Court, CourtPlayer
 from app.forms import PlayerForm, MatchForm
 
+
+
 @app.route('/')
 def layout():
     return render_template("layout.html")
@@ -29,8 +31,8 @@ def draft():
     return render_template('draft.html', players=players, form=form)
 
 
-@app.route('/add_player', methods=['GET', 'POST'])
-def add_player():
+#@app.route('/add_player', methods=['GET', 'POST'])
+#def add_player():
     form = PlayerForm()
     
     if form.validate_on_submit():
@@ -44,6 +46,7 @@ def add_player():
         return redirect(url_for('draft'))
     
     return render_template('add_player.html', form=form)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
