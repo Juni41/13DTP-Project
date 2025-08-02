18378a94f96f810b6b7f4f3faaa9471e02357bad
Justin Hsiao <hsiaojustin41@gmail.com>
2025-08-02 16:56:34 +1200

    backend logic for putting players together

--- Project/app/forms.py
+++ Project/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, IntegerField, RadioField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 
 class PlayerForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=30)])
@@ -17,10 +17,12 @@ class PlayerForm(FlaskForm):
     submit = SubmitField('Add Player')
 
 class MatchForm(FlaskForm):
-    num_courts = IntegerField('Number of Courts', validators=[DataRequired()])
-    method = RadioField('Sorting Method', choices=[
+    num_courts = IntegerField('Number of Courts', validators=[
+        DataRequired(), NumberRange(min=1, message="need at least 1 court")
+    ])
+    method = SelectField('Sorting Method', choices=[
         ('random', 'Random'),
         ('skill', 'Balance by Skill'),
         ('gender', 'Balance by Gender')
-    ], validators=[DataRequired()])
+    ])
     submit = SubmitField('Generate Matches')
--- Project/app/routes.py
+++ Project/app/routes.py
@@ -12,6 +12,10 @@ def layout():
 def homepage():
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 @app.route('/draft', methods=['GET', 'POST'])
 def draft():
     form = PlayerForm()
@@ -30,7 +34,40 @@ def draft():
 
 @app.route('/generate_matches', methods=['GET', 'POST'])
 def generate_matches():
+    form = MatchForm()
 
+    if form.validate_on_submit():
+        total_players = Player.query.count()
+        required_players = form.num_courts.data * 4
+
+        if total_players < required_players:
+            return render_template('generated_match.html', form=form, matches=None)
+
+        # Clear previous matches and players aaaaaaaaaaaaaaaaaa
+        CourtPlayer.query.delete()
+        Court.query.delete()
+        Match.query.delete()
+        db.session.commit()
+
+        players = Player.query.all()
+        match_data = create_matches(players, form.num_courts.data)
+
+        new_match = Match(num_courts=form.num_courts.data)
+        db.session.add(new_match)
+        db.session.flush()
+
+        for data in match_data:
+            court = Court(court_number=data['court'], match_id=new_match.id)
+            db.session.add(court)
+            db.session.flush()
+
+            for player in data['team1'] + data['team2']:
+                db.session.add(CourtPlayer(court_id=court.id, player_id=player.id))
+
+        db.session.commit()
+        return redirect(url_for('view_matches'))
+
+    return render_template('generated_match.html', form=form, matches=None)
 
 def create_matches(players, num_courts):
     available = list(players)
