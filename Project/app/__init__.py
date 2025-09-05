406836d581007a77396613382a0d3bfeaa763b7b
Justin Hsiao <hsiaojustin41@gmail.com>
2025-09-05 17:56:41 +1200

    added styling for playerlist

--- Project/app/__init__.py
+++ Project/app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///draft.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
